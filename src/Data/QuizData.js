4a5040b104c65e1405c37edaf2f5e94d2ad837e3
RashiMawandia <128953601+RashiMawandia@users.noreply.github.com>
2024-03-11 12:15:37 +0530

    initial commit

---
+++ src/Data/QuizData.js
@@ -0,0 +1,32 @@
+export const QuizData = [
+    {
+        question: "Which language runs in a web browser?",
+        options: ["Java", "C", "Python", "JavaScript"],
+        answer: 4
+    },
+    {
+        question: "What does CSS stand for?",
+        options: ["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
+        answer: 2
+    },
+    {
+        question: "What does HTML stand for?",
+        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborghinis"],
+        answer: 1
+    },
+    {
+        question: "What year was JavaScript launched?",
+        options: ["1996", "1995", "1994", "none of the above"],
+        answer: 2
+    },
+    {
+        question: "Which company developed the React JavaScript library?",
+        options: ["Facebook", "Google", "Microsoft", "Amazon"],
+        answer: 1
+    },
+    {
+        question: "What is the primary purpose of a 'git commit'?",
+        options: ["Add changes to the working directory", "Save changes to the local repository", "Push changes to the remote repository", "Undo the last commit"],
+        answer: 2
+    },
+];
